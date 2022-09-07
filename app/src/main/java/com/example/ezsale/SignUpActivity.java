3f8fff05d41712b50b49f23d975b2b8956c7e2c8
Mohammad Rahman <mohammadrahman497@yahoo.com>
2022-09-07 18:18:28 -0400

    added user authentication

--- app/src/main/java/com/example/ezsale/SignUpActivity.java
+++ app/src/main/java/com/example/ezsale/SignUpActivity.java
@@ -2,13 +2,75 @@ package com.example.ezsale;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        Button registerButton = findViewById(R.id.register_submit);
+        registerButton.setOnClickListener(view -> {
+            // Get registration form fields
+            String userName = ((EditText)findViewById(R.id.register_username)).getText().toString();
+            String email = ((EditText)findViewById(R.id.register_email)).getText().toString();
+            String password = ((EditText)findViewById(R.id.register_password)).getText().toString();
+
+            if (TextUtils.isEmpty(userName) || TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                Toast.makeText(SignUpActivity.this, "Incomplete form!", Toast.LENGTH_LONG).show();
+            } else if (password.length() < 6) {
+                Toast.makeText(SignUpActivity.this, "Password needs to be at least 6 characters!", Toast.LENGTH_LONG).show();
+            } else {
+                // If form is in good standing then register user
+                registerUsers(userName, email, password);
+            }
+        });
+
+    }
+
+    private void registerUsers(String userName, String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUpActivity.this, task -> {
+                if (task.isSuccessful()){
+                    Map<String, String> userFields = new HashMap<>();
+                    userFields.put("userName", userName);
+                    userFields.put("email", email);
+
+                    String uid = Objects.requireNonNull(task.getResult().getUser()).getUid();
+
+                    db.collection("users").document(uid).set(userFields).addOnCompleteListener(task1 -> {
+                        if (task1.isSuccessful()) {
+                            Toast.makeText(SignUpActivity.this, "Added User Fields!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                    Log.d("registered", "registration to firebase complete");
+                    Toast.makeText(SignUpActivity.this, "Registration Complete!", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(SignUpActivity.this, UserModesActivity.class));
+                }
+                else {
+                    Toast.makeText(SignUpActivity.this, "Registration FAILED - Email in use", Toast.LENGTH_LONG).show();
+                }
+        });
     }
 }
--- app/src/main/java/com/example/ezsale/UserModesActivity.java
+++ app/src/main/java/com/example/ezsale/UserModesActivity.java
@@ -2,7 +2,12 @@ package com.example.ezsale;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserModesActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class UserModesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_modes);
+
+        Button logoutButton = findViewById(R.id.logout_button);
+
+        logoutButton.setOnClickListener(view -> {
+            FirebaseAuth.getInstance().signOut();
+            Toast.makeText(UserModesActivity.this, "Logged Out!", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(UserModesActivity.this, MainActivity.class));
+        });
+
     }
 }
