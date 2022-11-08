eeba6bcece156ee1fa012da31c62d7934493436b
Mohammad Rahman <mohammadrahman497@yahoo.com>
2022-11-08 18:19:40 -0500

    made changes to email system

--- app/src/main/java/com/example/ezsale/SendEmailActivity.java
+++ app/src/main/java/com/example/ezsale/SendEmailActivity.java
@@ -3,6 +3,7 @@ package com.example.ezsale;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,8 @@ public class SendEmailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_email);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         emailAddress = findViewById(R.id.email_address_input);
         emailSubject = findViewById(R.id.email_subject_input);
@@ -29,6 +32,7 @@ public class SendEmailActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 sendMail();
+                finish();
             }
         });
 
@@ -36,15 +40,17 @@ public class SendEmailActivity extends AppCompatActivity {
 
     private void sendMail() {
         String email = emailAddress.getText().toString();
+        String[] emailList = email.split(",");
         String subject = emailSubject.getText().toString();
         String message = emailMessage.getText().toString();
 
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.putExtra(Intent.EXTRA_EMAIL, email);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.putExtra(Intent.EXTRA_EMAIL, emailList);
         intent.putExtra(Intent.EXTRA_SUBJECT, subject);
         intent.putExtra(Intent.EXTRA_TEXT, message);
 
-        intent.setType("message/rfc822");
+        //intent.setType("message/rfc822");
+        intent.setData(Uri.parse("mailto:"));
         startActivity(Intent.createChooser(intent, "Choose An Email Client"));
     }
 
