22febcaaec74f021880183ff69a04ba0aa578ee5
Mohammad Rahman <mohammadrahman497@yahoo.com>
2022-12-02 18:02:31 -0500

    added splash screen

---
+++ app/src/main/java/com/example/ezsale/generaluser/SplashScreenActivity.java
@@ -0,0 +1,32 @@
+package com.example.ezsale.generaluser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+import com.example.ezsale.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+    }
+}
